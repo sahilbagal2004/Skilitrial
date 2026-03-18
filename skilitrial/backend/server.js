2449e54e19a2d22a933ee3063ebd318fbf891e62
Sahil Satish Bagal <sahilbagal6445@gmail.com>
2026-03-18 16:32:07 +0530

    Update

--- skilitrial/backend/server.js
+++ skilitrial/backend/server.js
@@ -40,7 +40,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Explicitly handle preflight OPTIONS requests for all routes
-app.options("*", cors(corsOptions));
+app.options(/(.*)/, cors(corsOptions));
 
 /* ================= MIDDLEWARE ================= */
 
